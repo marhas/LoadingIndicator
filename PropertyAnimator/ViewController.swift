f465fef1ea4a616e51209335e694a5caa3381ef1
Marcel Hasselaar <marcel@hasselaar.nu>
2020-11-04 17:04:33 +0100

    Initial Commit

---
+++ PropertyAnimator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PropertyAnimator
+//
+//  Created by Marcel Hasselaar on 2020-11-04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
